885ad104f8fb34fff1e5fb99726422120c1b10a6
BhumicaPG <bhumigangurde8@gmail.com>
2023-09-19 11:51:06 +0530

    Updates

---
+++ precache-manifest.e8a1db50cf5dcae87ec4fa3416cffb52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa1e61748bdf9a58f7e589350fd04d9",
+    "url": "/CodePen-Clone/index.html"
+  },
+  {
+    "revision": "025c19159c7f85f03cad",
+    "url": "/CodePen-Clone/static/css/2.dc56b8e7.chunk.css"
+  },
+  {
+    "revision": "23d81876b6b0cd6ccfda",
+    "url": "/CodePen-Clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "025c19159c7f85f03cad",
+    "url": "/CodePen-Clone/static/js/2.5a228efd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodePen-Clone/static/js/2.5a228efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d81876b6b0cd6ccfda",
+    "url": "/CodePen-Clone/static/js/main.ba5ef7f4.chunk.js"
+  },
+  {
+    "revision": "dd8cb2888abc42f98df7",
+    "url": "/CodePen-Clone/static/js/runtime-main.759eee3b.js"
+  }
+]);
